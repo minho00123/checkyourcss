4181cc6fa9114c26ff4f8cfc0ad1a8dc54b7f866
Min Ho Jang <minho00123@gmail.com>
2024-02-01 11:37:30 +0900

    Feat: Create Boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#0D0D0D',
+      gray: '#D9D9D9',
+      'gray-200': '#D9D9D933',
+      yellow: '#F2B705',
+      'yellow-300': '#F2B7054D',
+      'yellow-200': '#F2B70533',
+      'yellow-100': '#F2B7051A',
+      green: '#47A603',
+      'green-200': '#47A60333',
+      red: '#F21F0C',
+      'red-200': '#F21F0C33',
+      'red-5': '#F21F0C0D'
+    }
+  },
+  plugins: []
+}
