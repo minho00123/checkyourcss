7a8f095a47891dbf85da5e40d596cd9356339a2a
imheeseung <gmltmd519@gmail.com>
2024-02-09 17:18:28 +0900

    Refactor: Change inline css to tailwind css

--- src/renderer/src/components/Header.jsx
+++ src/renderer/src/components/Header.jsx
@@ -6,13 +6,12 @@ function Header() {
   }
 
   return (
-    <header className="fixed top-0 w-full bg-gray z-40 flex justify-center p-8 border-b-2 shadow-md">
+    <header className="fixed top-0 w-full bg-gray z-40 flex justify-center p-8 border-b-2 shadow-md cursor-pointer">
       <img
         src={cycImage}
         alt="cyc logo"
         className="absolute -top-11 left-5 w-48"
         onClick={handleImageClick}
-        style={{ cursor: "pointer" }}
       />
       <h1 className="text-4xl font-bold drop-shadow-2xl">Check Your CSS</h1>
     </header>
